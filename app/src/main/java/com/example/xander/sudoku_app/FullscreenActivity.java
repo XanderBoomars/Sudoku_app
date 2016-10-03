1c2bf60f67dd76c68bfbc588131b00c0e73afb55
sateeprikker <boo.mars@hotmail.com>
2016-10-03 23:30:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/xander/sudoku_app/FullscreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.xander.sudoku_app;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class FullscreenActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
